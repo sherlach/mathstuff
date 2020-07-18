a8cf24f44c4e0776d7663c3e993ac29de2736916
Your Name <rrggbb@tutanota.com>
2020-07-18 23:47:44 +1000

    Cleaner code. Thanks nicey!

--- wordsquestion.py
+++ wordsquestion.py
@@ -1,22 +1,19 @@
-
 problem = input("What is the inital word? Not case-sensitive.\n").upper()
 
-letters = "".join(set(list(problem)))
+letters = "".join(set(problem))
 n = len(letters)
 p = int(input("How long should the product-words be? Give as single numeral.\n"))
 
 legalcharacters = {}
 
 for letter in letters:
-
-    legalcharacters.update({letter : problem.count(letter)})
+    legalcharacters[letter] = problem.count(letter)
 
 def checklegality(term):
-    legal = True
     for character in set(list(term)):
         if term.count(character) > legalcharacters[character]:
-            legal = False
-    return legal
+            return False
+    return True
 
 def cycle(delayfactor): #delayfactor changes the tempo - ie, a delayfactor of `n` means
                         #the iteration only progresses after n repetitions of the current step
@@ -28,8 +25,8 @@ def cycle(delayfactor): #delayfactor changes the tempo - ie, a delayfactor of `n
             
             response.append(letters[counterno])
             if iteration % delayfactor == 0:
-                counterno = counterno+1
-            iteration = iteration+1
+                counterno += 1
+            iteration += 1
     
     return response
 
@@ -47,6 +44,6 @@ for i in range(p**n):
     if checklegality(entry):
         answer.append(entry) 
 
-print("Total number of valid words made: " +str(len(answer)))
+print("Total number of valid words made:", str(len(answer)))
 print(answer)
 
