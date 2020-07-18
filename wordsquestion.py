b559f39b02471a19677cf4b3d7dfbf4fe718ffa1
nicey0 <53410840+nicey0@users.noreply.github.com>
2020-07-18 10:44:54 -0300

    Engooden code
    Like u asked me to :D

--- wordsquestion.py
+++ wordsquestion.py
@@ -1,52 +1,44 @@
+problem = input("What is the inital word? Not case-sensitive.\n"> ).upper()
 
-problem = input("What is the inital word? Not case-sensitive.\n").upper()
-
-letters = "".join(set(list(problem)))
+letters = "".join(set(problem))
 n = len(letters)
-p = int(input("How long should the product-words be? Give as single numeral.\n"))
+p = int(input("How long should the product-words be? Give as single numeral.\n> "))
 
 legalcharacters = {}
 
 for letter in letters:
-
-    legalcharacters.update({letter : problem.count(letter)})
+    legalcharacters[letter] = problem.count(letter)
 
 def checklegality(term):
-    legal = True
-    for character in set(list(term)):
+    for character in set(term):
         if term.count(character) > legalcharacters[character]:
-            legal = False
-    return legal
+            return False
+    return True
 
-def cycle(delayfactor): #delayfactor changes the tempo - ie, a delayfactor of `n` means
-                        #the iteration only progresses after n repetitions of the current step
-    response=[]  
+def cycle(delayfactor): # delayfactor changes the tempo - ie, a delayfactor of `n` means
+                        # the iteration only progresses after n repetitions of the current step
+    response = []
     while len(response) < p**n: #n**n before
         counterno = 0
         iteration = 1
         while counterno < n:
-            
             response.append(letters[counterno])
             if iteration % delayfactor == 0:
-                counterno = counterno+1
-            iteration = iteration+1
-    
+                counterno += 1
+            iteration += 1
     return response
 
 
 answer=[]
 
 for i in range(p**n):
-    word=[]
-    
+    word = []
     for k in range(p):
         word.append(cycle(n**k)[i])
-       
-    entry="".join(word)
 
+    entry = "".join(word)
     if checklegality(entry):
-        answer.append(entry) 
+        answer.append(entry)
 
-print("Total number of valid words made: " +str(len(answer)))
+print("Total number of valid words made:", len(answer))
 print(answer)
-
